b4ec2dd086d0ebf5520bdb5cdb49bbb8407037ff
rosemei1 <hanifirfan414@gmail.com>
2022-12-03 00:28:16 +0700

    baru

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -37,7 +37,7 @@
                     </a>
 
                     <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                        @csrf
+                        {{ csrf_field() }}
                     </form>
                 </div>
             </li>
